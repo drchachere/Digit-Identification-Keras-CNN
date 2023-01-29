909a8561e7ba43dc6efd4fb3d3fec3c41a79e88c
drchachere <drchachere@gmail.com>
2023-01-29 00:09:38 -0500

    CNN script using SVHN dataset, 60% validation accuracy

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Digit-Identification-Keras-CNN
-Convolutional neural network model used to predict digits from house number photos
+
+In this project I built a convolutional neural network model used to predict digits from house number photos.  Keras and Tensorflow libraries are used to create the model.
+
+The house number photos are from Stanford University's The Street View House Numbers (SVHN) Dataset.  Please find the link to this dataset, as well as more information about it, [here](http://ufldl.stanford.edu/housenumbers/).
---
+++ digit_identifier.py
@@ -0,0 +1,66 @@
+import numpy as np
+from tensorflow import keras
+import scipy.io
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.utils import np_utils
+import sklearn
+from sklearn.model_selection import train_test_split
+
+# Load the MNIST dataset
+mat_file = scipy.io.loadmat('train_32x32.mat')
+X = mat_file['X']
+y = mat_file['y']
+
+# re-shuffling the dimensions of the features
+# changing from X(rows, cols, color channles, samples) to X(samples, rows, cols, color channels)
+X = np.transpose(X, (3, 0, 1, 2))
+m = X.shape[0]
+
+# re-labeling 10's to 0's in the labels
+y = list(map(lambda x: 0 if x==10 else x, y))
+y = np.array(y).reshape(m, 1)
+
+# print(X.shape) # check the shape of X
+# print(y.shape) # check the shape of y
+
+# Split the datasets
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# Reshape the data
+X_train = X_train.reshape(X_train.shape[0], 32, 32, 3)
+X_test = X_test.reshape(X_test.shape[0], 32, 32, 3)
+
+# Normalize the data
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+# One-hot encode the labels
+y_train = np_utils.to_categorical(y_train, 10)
+y_test = np_utils.to_categorical(y_test, 10)
+
+# Create the model
+model = Sequential()
+model.add(Conv2D(50, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(16, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Fit the model
+
+# model.fit(X_train, y_train, batch_size=250, epochs=100, verbose=1, validation_data=(X_test, y_test))
+# test accuracy = .7896
+
+model.fit(X_train, y_train, batch_size=500, epochs=80, verbose=1, validation_data=(X_test, y_test))
+
+# Evaluate the model
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
